eff2aa44e76f5ed48c5e0fba39b37984ea697845
George Lee <keoki.lee@gmail.com>
2015-04-20 21:25:13 -1000

    Implement step 1. Create navigator component with initial component.

---
+++ AppWithAMap/index.ios.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+} = React;
+
+var AppWithAMap = React.createClass({
+  render() {
+    return (
+      <NavigatorIOS style={styles.navigator} initialRoute={{
+        component: Welcome,
+        title: "Welcome"
+      }} />
+    )
+  }
+});
+
+var Welcome = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  navigator: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AppWithAMap', () => AppWithAMap);
